bd6ce62ffaab93645f000c717cfadab17322a1ec
Artem Grinko <asgrinko@alfabank.ru>
2021-06-14 15:32:45 +0300

    управление, обработка врезания в себя

--- script.js
+++ script.js
@@ -2,12 +2,23 @@ let height;
 let width;
 const rows = 10;
 const columns = 10;
+// для теста длина змеи = 4
 const snake = [{
     row: 5,
     column: 0
+}, {
+    row: 5,
+    column: 1
+}, {
+    row: 5,
+    column: 2
+}, {
+    row: 5,
+    column: 3
 }];
 let direction = "RIGHT";
 let game;
+let interval;
 
 window.onload = () => {
     console.log("Скрипты подключены");
@@ -17,7 +28,7 @@ window.onload = () => {
     width = height  * 1.5;
     canvas.width = width;
     canvas.height = height;
-    setInterval(() => {
+    interval = setInterval(() => {
         moveSnake()
         render();
     }, 1000);
@@ -42,14 +53,70 @@ function moveSnake() {
     let head = getHead();
     switch (direction) { // добавляем новый элемент после головы
         case "RIGHT":
-            snake.push({
-                row: head.row,
-                column: head.column + 1
-            });
+            addBody(head.row, head.column + 1);
+            break;
+        case "LEFT":
+            addBody(head.row, head.column - 1);
+            break;
+        case "UP":
+            addBody(head.row - 1, head.column);
+            break;
+        case "DOWN":
+            addBody(head.row + 1, head.column);
+            break;
     }
     snake.shift(); // удаляем хвостовой элемент
 }
 
+function addBody(row, column) {
+    if (isCellFill(row, column)) {
+        gameover();
+    }
+    snake.push({
+        row,
+        column
+    });
+}
+
+function isCellFill(row, column) {
+    return !!snake.find(it => it.row === row && it.column === column);
+}
+
 function getHead() {
     return snake[snake.length - 1];
+}
+
+function gameover() {
+    console.log("Конец игры");
+    clearInterval(interval);
+}
+
+document.addEventListener("keydown", onKeyDown, false);
+
+function changeDirection(dir) {
+    console.log(dir);
+    // запрещаем поворот на 180 градусов
+    if (dir === "RIGHT" && direction === "LEFT") return;
+    if (dir === "LEFT" && direction === "RIGHT") return;
+    if (dir === "UP" && direction === "DOWN") return;
+    if (dir === "DOWN" && direction === "UP") return;
+    direction = dir;
+}
+
+function onKeyDown(event) {
+    var key = event.key;
+    switch (key) {
+        case "d":
+            changeDirection("RIGHT");
+            break;
+        case "s":
+            changeDirection("DOWN");
+            break;
+        case "a":
+            changeDirection("LEFT");
+            break;
+        case "w":
+            changeDirection("UP");
+            break;
+    }
 }
